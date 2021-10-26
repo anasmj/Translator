b8c396bd1fe4f8abd5ddaa6f13116bcc3b76c1f1
Anas Al Azmi <anasmj7@gmail.com>
2021-10-25 20:06:58 -0700

    fix: Icon column are saperated from text field so no color change occurs

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,6 +4,8 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
 import 'package:code_language/providers/code_provider.dart';
 import 'package:clipboard/clipboard.dart';
+import 'package:share/share.dart';
+
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -67,7 +69,17 @@ class _MainPageState extends State<MainPage> {
             child: Container(
               height: screenHeight * 0.40,
               color: Colors.white,
-              child: getUpperTextField(),
+              //TODO:  make icon column out of the text field to stay consistent
+              //child: getUpperTextField(),
+              child: Stack(
+                children: [
+                  getUpperTextField(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                      child: startTextController.text.isEmpty ?  iconColumnWithoutText():  iconColumnWithText(),
+                  ),
+                ],
+              ),
             ),
           ),
           ///2ND CONTAINER
@@ -76,7 +88,16 @@ class _MainPageState extends State<MainPage> {
             child: Container(
               height: screenHeight * 0.42,
               color: Colors.white,
-              child:  getBottomTextField(),
+              //child:  getBottomTextField(),
+              child: Stack(
+                children: [
+                  getBottomTextField(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: targetTextController.text.isEmpty ? Container(width: 0,): iconColumnForResult(),
+                  )
+                ],
+              ),
             ),
           ),
         ],
@@ -86,7 +107,6 @@ class _MainPageState extends State<MainPage> {
 
   Widget getUpperTextField(){
     final code = Provider.of<CodeProvider>(context);
-
     return TextFormField(
       controller: startTextController,
       onChanged: (value) {
@@ -98,18 +118,17 @@ class _MainPageState extends State<MainPage> {
       maxLines: 16,
       cursorColor: Colors.blue,
       cursorHeight: 30,
-      decoration:  InputDecoration(
-        suffixIcon: startTextController.text.isEmpty ?  iconColumnWithoutText():  iconColumnWithText(),
-        contentPadding: const EdgeInsets.only(top: 10, left: 10),
+      decoration:  const InputDecoration(
+        //suffixIcon: startTextController.text.isEmpty ?  iconColumnWithoutText():  iconColumnWithText(),
+        contentPadding: EdgeInsets.only(top: 10, left: 10,right: 32),
         border: InputBorder.none,
         hintText: "Text",
         //filled: true,
-        hintStyle: const TextStyle(fontSize: 20, color: Colors.grey),
+        hintStyle: TextStyle(fontSize: 20, color: Colors.grey),
       ),
     );
   }
   Widget getBottomTextField (){
-
     TextEditingController targetTextController = TextEditingController();
     final code = Provider.of<CodeProvider>(context);
     targetTextController.text = code.targetText.isNotEmpty? code.targetText: '';
@@ -123,12 +142,12 @@ class _MainPageState extends State<MainPage> {
       maxLines: 16,
       cursorColor: Colors.blue,
       cursorHeight: 30,
-      decoration:  InputDecoration(
-        suffixIcon: targetTextController.text.isEmpty ? Container(width: 0,): iconColumnForResult(),
-        contentPadding: const EdgeInsets.only(top: 10, left: 10),
+      decoration:  const InputDecoration(
+        //suffixIcon: targetTextController.text.isEmpty ? Container(width: 0,): iconColumnForResult(),
+        contentPadding: EdgeInsets.only(top: 10, left: 32),
         border: InputBorder.none,
         //filled: true,
-        hintStyle: const TextStyle(fontSize: 20, color: Colors.grey),
+        hintStyle: TextStyle(fontSize: 20, color: Colors.grey),
       ),
     );
 
@@ -140,9 +159,11 @@ class _MainPageState extends State<MainPage> {
         IconButton(
           onPressed: ()async {
             final value = await FlutterClipboard.paste();
-            //startTextController.selection = TextSelection.fromPosition(TextPosition(offset: startTextController.text.length));
-
             startTextController.text = value;
+            startTextController.selection = TextSelection.fromPosition(TextPosition(offset: startTextController.text.length));
+            setState(() {
+              ///resolve issue for not having the 'iconColumnWithText' icons at the right as soon as something pasted in text field
+            });
           },
           icon: const Icon(Icons.paste_rounded),
         ),
@@ -166,6 +187,7 @@ class _MainPageState extends State<MainPage> {
           onPressed: () {
             code.clearText();
             startTextController.clear();
+            targetTextController.clear();
           },
           icon: const Icon(
             Icons.cancel,
@@ -181,7 +203,8 @@ class _MainPageState extends State<MainPage> {
             iconSize: 30),
         IconButton(
           onPressed:() async {
-            //TODO fix bug on paste
+            //TODO fix bug: replace existed text with value on clipboard
+            startTextController.selection = TextSelection.fromPosition(TextPosition(offset: startTextController.text.length));// move cursor if there are already some text
             final value = await FlutterClipboard.paste();
             startTextController.text = value;
           },
@@ -202,7 +225,7 @@ class _MainPageState extends State<MainPage> {
           onPressed: () {
             code.targetText = startTextController.text;
             targetTextController.text = startTextController.text;
-            FocusScope.of(context).unfocus();
+            FocusScope.of(context).unfocus(); //removes keyboard after pressing enter
           },
           icon: const Icon(
             Icons.subdirectory_arrow_left,
@@ -214,13 +237,20 @@ class _MainPageState extends State<MainPage> {
   Widget iconColumnForResult(){
     return Column(
       children:  [
-        IconButton(
-          onPressed: () {
-          },
-          icon: const Icon(Icons.copy_outlined),
+        Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: () async {
+                await FlutterClipboard.copy(startTextController.text);
+                toast('copied');
+              },
+              icon: const Icon(Icons.copy_outlined),
+            );
+          }
         ),
         IconButton(
           onPressed: () {
+            Share.share(targetTextController.text.toString());
           },
           icon: const Icon(
             Icons.share,
